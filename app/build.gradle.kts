cd9af5e4c33cc5c5657bdcf6a50c4a56034162fa
jozhikbeznozhek-dev <jozhikbeznozhek-dev@users.noreply.github.com>
2026-07-20 05:47:51 +0300

    v2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.hermes.downloader"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "2.0.0"
+        versionCode = 4
+        versionName = "2.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk { abiFilters += listOf("arm64-v8a") }
